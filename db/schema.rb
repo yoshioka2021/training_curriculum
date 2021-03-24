b93eb1b6e33089310229e9321226eff51900f4a8
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-03-24 16:48:02 +0900

    Issue4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_072724) do
+ActiveRecord::Schema.define(version: 2021_03_24_073116) do
+
+  create_table "calenders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "plan"
+    t.string "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "plan"
